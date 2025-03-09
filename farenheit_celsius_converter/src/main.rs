7c8994e88a7629d85ec63a6b73a5d9e8b56860f3
Igor Avilez <igor.avilez@pm.me>
2025-03-09 15:34:26 -0300

    finalizacao conversor de temperatura

--- farenheit_celsius_converter/src/main.rs
+++ farenheit_celsius_converter/src/main.rs
@@ -1,26 +1,67 @@
 use std::io;
 fn main() {
     println!("Olá, seja bem vindo ao conversor!");
-    loop {
+    'main_loop: loop {
         println!("Se você deseja converter um valor de Fahrenheit para Celsius, digite 1.");
         println!("Se você deseja converter um valor de Celsius para Fahrenheit, digite 2.");
         println!("Para sair, digite 3.");
-        let mut comando = String::new();
-        io::stdin().read_line(&mut comando).expect("Falha ao ler o comando.");
-        let comando: u8 = match comando.trim().parse() {
-            Ok(num) => {if num <= 3 {num} else {
-                println!("Por favor, informe um número válido.");
-                continue;
-            }},
+        let mut command = String::new();
+        io::stdin().read_line(&mut command).expect("Falha ao ler o comando.");
+        let command: u8 = match command.trim().parse() {
+            Ok(num) => num,
             Err(_) => {
                 println!("Por favor, informe um número válido.");
-                continue;
+                continue 'main_loop;
             },
         };
-        if comando == 3 {
-            println!("Até a próxima!");
+
+        let original_unity: &str;
+        let new_unity: &str;
+        let converter: fn(f32) -> f32;
+
+        match command {
+            1 => {
+                original_unity = "Fahrenheit";
+                new_unity = "Celsius";
+                converter = fahrenheit_to_celsius;
+            },
+            2 => {
+                original_unity = "Celsius";
+                new_unity = "Fahrenheit";
+                converter = celsius_to_fahrenheit;
+            },
+            3 => {
+                println!("Até a próxima!");
+                break 'main_loop;
+            },
+            _ => {
+                println!("Por favor, informe um número válido.");
+                continue 'main_loop;
+            },
+        }
+
+        loop {
+            println!("Digite a temperatura em graus {original_unity}:");
+            let mut temperature = String::new();
+            io::stdin().read_line(&mut temperature).expect("Falha ao ler o comando.");
+            let temperature: f32 = match temperature.trim().parse() {
+                Ok(num) => num,
+                Err(_) => {
+                    println!("Por favor, informe um número válido.");
+                    continue 'main_loop;
+                },
+            };
+            let result = converter(temperature);
+            println!("A temperatura convertida é de aproximadamente {result} graus {new_unity}.\n\n");
             break;
         }
-        println!("O comando dado foi {comando}")
     }
+}
+
+fn fahrenheit_to_celsius(temp: f32) -> f32 {
+    ((temp - 32.0) * 5.0/9.0).round()
+}
+
+fn celsius_to_fahrenheit(temp: f32) -> f32 {
+    ((temp * 9.0/5.0) + 32.0).round()
 }
